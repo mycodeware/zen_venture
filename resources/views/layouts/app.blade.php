55b0205f6f9e5e8b0e9548a37c9e980d5e22aadc
kandy <krsdata.net@gmail.com>
2019-03-13 16:10:13 +0000

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
     if($segment != 'faq')
     {
     @endphp
-        <script src="{{ url('js/app.js') }}" defer></script>
+//        <script src="{{ url('js/app.js') }}" defer></script>
     @php
     }
     else{
@@ -77,14 +77,9 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
-    <!-- Fontawesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-    <!-- Styles -->
-
-
-  <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
-
+  <link rel="stylesheet" href="{{ url('css/app.css') }}">
+<?php /*
   <link rel="stylesheet" href="{{ url('css/bootsnav.css') }}">
 
   <link rel="stylesheet" type="text/css" href="{{ url('css/font-awesome.min.css') }}">
@@ -94,7 +89,7 @@
   <link rel="stylesheet" type="text/css" href="{{ url('css/owl.carousel.min.css') }}">
 
   <link rel="stylesheet" type="text/css" href="{{ url('css/main.css') }}">
-
+*/?>
     @if (isset($recaptcha) && $recaptcha === true)
         <script src='https://www.google.com/recaptcha/api.js?render={{ env("GRECAPTCHA_SITE_KEY") }}'></script>
     @endif
@@ -323,13 +318,8 @@
 
   </footer>
 
-  <script src="{{ url('js/jquery.min.js') }}"></script>
+  <script src="{{ url('js/app.js') }}"></script>
 
-  <script src="{{ url('js/bootstrap.min.js') }}"></script>
-
-  <script src="{{ url('js/bootsnav.js') }}"></script>
-
-  <script src="{{ url('js/owl.carousel.min.js') }}"></script>
 
   <script type="text/javascript">
 
